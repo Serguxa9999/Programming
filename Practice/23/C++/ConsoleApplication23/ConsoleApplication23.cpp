7cf72aadeeaf648b3dfe4b244f7ca4c253d24673
Serguxa9999 <svp.perepechaev03@gmail.com>
2020-11-12 22:11:50 +0300

    first commit

---
+++ Practice/23/C++/ConsoleApplication23/ConsoleApplication23.cpp
@@ -0,0 +1,23 @@
+﻿#pragma first
+#include<iostream>
+using namespace std;
+int Factorial(int x);
+void main()
+{
+	int number;
+	cout << "number: " << endl;
+	cin>> number;
+	for (int x = 0; x <= number; x++)
+	{
+		cout << x << "!" << " = " << Factorial(x) << endl;
+	}
+}
+int Factorial(int x)
+{
+	int otvet = 1;
+	for (int i = 1; i <= x; i++)
+	{
+		otvet = otvet * i;
+	}
+	return otvet;
+}
